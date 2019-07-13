a89281ffe2e0db8d89c1291a0c02c962bc2bb487
NiceSlice <tanjamiskovic1@gmail.com>
2019-07-13 18:51:33 +0200

    Updates

---
+++ precache-manifest.851f9597b8b5f9ebf5dc43ded037f014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b775b1998314580fc5a576bc10202fbd",
+    "url": "/react-todo-practice/index.html"
+  },
+  {
+    "revision": "d77a1c58145239525efe",
+    "url": "/react-todo-practice/static/css/main.8b7f5935.chunk.css"
+  },
+  {
+    "revision": "11067b96a567c490eefb",
+    "url": "/react-todo-practice/static/js/2.737d708f.chunk.js"
+  },
+  {
+    "revision": "d77a1c58145239525efe",
+    "url": "/react-todo-practice/static/js/main.d1fa901c.chunk.js"
+  },
+  {
+    "revision": "f10705e9dbb200d8d74b",
+    "url": "/react-todo-practice/static/js/runtime~main.fe00543b.js"
+  },
+  {
+    "revision": "889e63b0300bef2e9867fbf08a5cbf3b",
+    "url": "/react-todo-practice/static/media/logo.889e63b0.svg"
+  }
+]);
